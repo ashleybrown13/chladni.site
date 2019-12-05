3110b98044d9785d752aaa095632bba2ed3a82db
ashleybrown13 <55456206+ashleybrown13@users.noreply.github.com>
2019-12-05 12:11:32 -0800

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,67 @@
+
+let rain
+let wind
+let highway
+let sink
+let boiling_water
+let vacuum
+let footsteps
+let crowd
+let car_start
+let rats
+let song
+let dryer
+let washing_machine
+let campus
+let fan
+let typing
+
+function setup() {
+
+	createCanvas(700, 700)
+
+
+}
+
+function preload() {
+	wind = loadSound('wind.mp3')
+	fan = loadSound('Fan.mp3')
+	campus = loadSound('campus_quiet.mp3')
+	rain = loadSound('rain.mp3')
+	song = loadSound('LesDeuxPianos.mp3')
+
+
+}
+
+
+
+function playWind() {
+
+	wind.play()
+
+}
+
+function playRain() {
+	
+	rain.play()
+
+}
+
+function playFan() {
+	
+	fan.play()
+
+}
+
+function playCampus() {
+	
+	campus.play()
+
+}
+
+function playSong() {
+	
+	song.play()
+
+}
+
